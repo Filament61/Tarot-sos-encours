a3864ddfa20e4bb56cf61ce8a634ce674f9695c6
Serge <sglemac@free.fr>
2019-06-13 17:20:57 +0200

    Initial Commit

---
+++ Tarot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tarot
+//
+//  Created by Serge Gori on 13/06/2019.
+//  Copyright © 2019 Serge Gori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
