d8f610747338d983b870512898d3b2d8b6d94380
Filament61 <kopacite@free.fr>
2019-06-25 20:16:15 +0200

    Ok

---
+++ Tarot/Model/PointsJeu.swift
@@ -0,0 +1,14 @@
+//
+//  PointsJeu.swift
+//  Tarot
+//
+//  Created by Serge Gori on 25/06/2019.
+//  Copyright © 2019 Serge Gori. All rights reserved.
+//
+
+import CoreData
+
+class PointsJeu: NSManagedObject {
+    
+    
+}
